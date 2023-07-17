ca5137e791a310029915cae8cca64150947701ba
Maku459 <hzk.butterfly459@gmail.com>
2023-07-17 18:29:45 +0900

    feat: Cardコンポーネントを追加

---
+++ src/app/data.ts
@@ -0,0 +1,20 @@
+export type Work = {
+  id: string;
+  title: string;
+  imageSrc: string;
+};
+
+const data: Work[] = [
+  {
+    id: "1",
+    title: "Work 01",
+    imageSrc: "/01.png",
+  },
+  {
+    id: "2",
+    title: "Work 02",
+    imageSrc: "/02.png",
+  },
+];
+
+export default data;
--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,9 +1,13 @@
-import Link from "next/link";
+import swagData from "../data";
+import Card from "./components/Card/Card";
 
 export default function Works() {
+  const data = swagData;
   return (
-    <div>
-      <p>Hello World!</p>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 auto-rows-max	 gap-6 m-10">
+      {data.map(({ id, title, imageSrc }) => (
+        <Card key={id} title={title} imageSrc={imageSrc}></Card>
+      ))}
     </div>
   );
 }
